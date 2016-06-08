a3725040dd264f45c13a8e6b5104e6928dd01b41
Alex Polozov <polozov@cs.washington.edu>
2016-06-07 17:36:00 -0700

    #2: SubStr + PosPair + AbsPos, string-based.
    ; Conflicts:
    ;	ProseTutorial.Substrings/ProseTutorial.Substrings.csproj
    ;	ProseTutorial.Substrings/ProseTutorial.Substrings.grammar
    ;	ProseTutorial.Substrings/Semantics.cs
    ;	ProseTutorial.Substrings/packages.config
    ;	ProseTutorial.TextExtraction/ProseTutorial.TextExtraction.csproj
    ;	ProseTutorial/App.config
    ;	ProseTutorial/Program.cs
    ;	ProseTutorial/ProseTutorial.csproj
    ;	ProseTutorial/packages.config

--- ProseTutorial/Program.cs
+++ ProseTutorial/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.ProgramSynthesis;
+using Microsoft.ProgramSynthesis.AST;
 using Microsoft.ProgramSynthesis.Extraction.Text.Semantics;
 using Microsoft.ProgramSynthesis.Compiler;
 using Microsoft.ProgramSynthesis.Diagnostics;
@@ -13,6 +16,21 @@ namespace ProseTutorial
 {
     internal static class Program
     {
-        private static void Main(string[] args) { }
+        private static void Main(string[] args)
+        {
+            LoadAndTestSubstrings();
+        }
+
+        private static void LoadAndTestSubstrings()
+        {
+            var grammar = ProseTutorial.Substrings.Language.Grammar;
+            if (grammar == null) return;
+
+            ProgramNode p = ProgramNode.Parse(@"SubStr(v, PosPair(AbsPos(v, -4), AbsPos(v, -1)))",
+                                              grammar, ASTSerializationFormat.HumanReadable);
+            const string data = "Microsoft PROSE SDK";
+            State input = State.Create(grammar.InputSymbol, data);
+            Console.WriteLine(p.Invoke(input));
+        }
     }
 }
