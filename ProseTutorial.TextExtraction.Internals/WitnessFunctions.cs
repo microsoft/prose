3a7e0608490683b5d2a18f51225378e343d95013
Alex Polozov <polozov@cs.washington.edu>
2017-01-20 20:42:22 -0800

    #9d: WitnessLinesMap skeleton
    ; Conflicts:
    ;	ProseTutorial.TextExtraction/ProseTutorial.TextExtraction.csproj
    ;	ProseTutorial.TextExtraction/WitnessFunctions.cs

--- ProseTutorial.TextExtraction.Internals/WitnessFunctions.cs
+++ ProseTutorial.TextExtraction.Internals/WitnessFunctions.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ProgramSynthesis;
+using Microsoft.ProgramSynthesis.Extraction.Text.Semantics;
+using Microsoft.ProgramSynthesis.Learning;
+using Microsoft.ProgramSynthesis.Rules;
+using Microsoft.ProgramSynthesis.Specifications;
+using static Microsoft.ProgramSynthesis.Extraction.Text.Semantics.Semantics;
 
 namespace ProseTutorial.TextExtraction
 {
-    public static class WitnessFunctions { }
+    public class WitnessFunctions : DomainLearningLogic
+    {
+        public WitnessFunctions(Grammar grammar) : base(grammar) { }
+
+        [WitnessFunction("LinesMap", 1)]
+        public static PrefixSpec WitnessLinesMap(GrammarRule rule, PrefixSpec spec)
+        {
+            var linesExamples = new Dictionary<State, IEnumerable<object>>();
+            foreach (State input in spec.ProvidedInputs)
+            {
+                var document = ((StringRegion)input[rule.Grammar.InputSymbol]);
+                var selectionPrefix = spec.PositiveExamples[input].Cast<StringRegion>();
+
+                linesExamples[input] = null; // <== deduce a prefix of line examples here
+            }
+            return new PrefixSpec(linesExamples);
+        }
+
+        [ExternLearningLogicMapping("Selection")]
+        public DomainLearningLogic ExternWitnessFunction
+            => new Substrings.WitnessFunctions(Substrings.Language.Grammar);
+    }
 }
