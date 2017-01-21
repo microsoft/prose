29124aeef78c292acb58bcea716cda629590aac2
Alex Polozov <polozov@cs.washington.edu>
2017-01-20 20:32:17 -0800

    #9a: FE grammar with SplitLines, without selection
    ; Conflicts:
    ;	ProseTutorial.TextExtraction/ProseTutorial.TextExtraction.csproj
    ;	ProseTutorial.TextExtraction/packages.config
    ;	ProseTutorial.sln

--- ProseTutorial.TextExtraction.Internals/RankingScore.cs
+++ ProseTutorial.TextExtraction.Internals/RankingScore.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.ProgramSynthesis.Extraction.Text.Semantics;
 using Microsoft.ProgramSynthesis;
+using Microsoft.ProgramSynthesis.AST;
+using Microsoft.ProgramSynthesis.Extraction.Text.Semantics;
 
 namespace ProseTutorial.TextExtraction
 {
-    public static class RankingScore
+    public class RankingScore : Feature<double>
     {
-        public const double VariableScore = 0;
+        public RankingScore(Grammar grammar) : base(grammar, "Score") { }
+
+        protected override double GetFeatureValueForVariable(VariableNode variable) => 0;
 
         [FeatureCalculator("SplitLines")]
         public static double Score_SplitLines(double document) => document;
 
         [FeatureCalculator("LinesMap")]
         public static double Score_LinesMap(double selection, double lines) => selection + lines + Token.MinScore * 20;
+
+        [FeatureCalculator("selection", Method = CalculationMethod.FromProgramNode)]
+        public static double Dummy(ProgramNode p) => 0;
     }
 }
---
+++ ProseTutorial.TextExtraction.Internals/WitnessFunctions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProseTutorial.TextExtraction
+{
+    public static class WitnessFunctions { }
+}
