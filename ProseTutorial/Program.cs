4bde3f47d58131883882849145766142d45b4ee9
Alex Polozov <polozov@cs.washington.edu>
2017-01-20 20:21:09 -0800

    #6e: Email benchmark for FF

--- ProseTutorial/Program.cs
+++ ProseTutorial/Program.cs
@@ -1,15 +1,10 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Microsoft.ProgramSynthesis;
 using Microsoft.ProgramSynthesis.AST;
-using Microsoft.ProgramSynthesis.Extraction.Text.Semantics;
-using Microsoft.ProgramSynthesis.Compiler;
-using Microsoft.ProgramSynthesis.Diagnostics;
 using Microsoft.ProgramSynthesis.Specifications;
 using Microsoft.ProgramSynthesis.Specifications.Extensions;
-using Microsoft.ProgramSynthesis.Learning;
 using static ProseTutorial.Utils;
 
 namespace ProseTutorial
@@ -35,6 +30,28 @@ namespace ProseTutorial
             string sdk = data.Substring(data.Length - 3);
             Spec spec = ShouldConvert.Given(grammar).To(data, sdk);
             Learn(grammar, spec);
+
+            TestFlashFillBenchmark(grammar, "emails");
+        }
+
+        private static void TestFlashFillBenchmark(Grammar grammar, string benchmark, int exampleCount = 2)
+        {
+            string[] lines = File.ReadAllLines($"benchmarks/{benchmark}.tsv");
+            Tuple<string, string>[] data = lines.Select(l =>
+            {
+                var parts = l.Split(new[] { "\t" }, StringSplitOptions.RemoveEmptyEntries);
+                return Tuple.Create(parts[0], parts[1]);
+            }).ToArray();
+            var examples = data.Take(exampleCount)
+                               .ToDictionary(t => State.Create(grammar.InputSymbol, t.Item1),
+                                             t => (object) t.Item2);
+            var spec = new ExampleSpec(examples);
+            ProgramNode program = Learn(grammar, spec);
+            foreach (Tuple<string, string> row in data.Skip(exampleCount))
+            {
+                var output = program.Invoke(State.Create(grammar.InputSymbol, row.Item1));
+                WriteColored(ConsoleColor.DarkCyan, $"{row.Item1} => {output}");
+            }
         }
     }
 }
